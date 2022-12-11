a1f5b361eba508a683fb4176a3ad1ed07416df78
mrtwumgh <twumstephen2006@gmail.com>
2022-12-11 11:17:49 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    num_args = len(sys.argv) - 1
-    print("{} arguments:".format(num_args))
+    num_args = len(sys.argv)
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args - 1))
+
     if num_args > 0:
-        for i, args in enumerate(sys.argv):
-            if args == (sys.argv[0]):
+        for i, arg in range(1, sys.argv):
+            if i == 0:
                 continue
-            print("{}: {}".format(i, args))
+            print("{}: {}".format(i, arg))
+    else:
+        print(".")
