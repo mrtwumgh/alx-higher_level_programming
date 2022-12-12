5e184ca324804197e30b6151d99697b4048ce2ad
mrtwumgh <twumstephen2006@gmail.com>
2022-12-12 06:51:56 +0000

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == 0:
+        return None
+    else:
+        max_value = my_list[0]
+        for element in my_list:
+            if element > max_value:
+                max_value = element
+    return max_value
