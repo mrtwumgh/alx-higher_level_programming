223147c945a9ba805b45e5aff4ef35d78a8e69d7
mrtwumgh <twumstephen2006@gmail.com>
2022-12-25 14:26:12 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed = 0
+        for i in range(x):
+            print(my_list[i], end='')
+            printed += 1
+        print()
+    except:
+        print()
+        return printed
+    else:
+        return printed
