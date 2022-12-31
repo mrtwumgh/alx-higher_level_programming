896ab248d69eff538ca07439cb8a629396091ddd
mrtwumgh <twumstephen2006@gmail.com>
2022-12-31 10:19:41 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines the class: Square"""
+
+
+class Square:
+    """
+    A class that represents a square
+    
+    Attributes:
+        __size(int): The size of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes the attributes of square
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
