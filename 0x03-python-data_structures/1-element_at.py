527e25fc6e4b645a88f9c858e1b7f46399993c84
mrtwumgh <twumstephen2006@gmail.com>
2023-06-12 06:56:29 +0000

    refactored code on 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
-    for x in my_list:
-        if x == idx:
-            return my_list[x]
+    return my_list[idx]
