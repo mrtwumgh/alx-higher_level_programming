71a558914dbadd678f98759a432fbaa5aac59d92
mrtwumgh <twumstephen2006@gmail.com>
2023-06-12 09:00:51 +0000

    8-multiple_returns.py script added

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
+    if sentence == '':
         sentence[0] = None
     len_s = len(sentence)
     return len_s, sentence[0]
