f758ebed07a513ac83c410c356289e9a9ae4206e
mrtwumgh <twumstephen2006@gmail.com>
2023-06-12 09:35:15 +0000

    7-add_tuple.py script added

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    a1, a2 = 0, 0
+    b1, b2 = 0, 0
+
+    if len(tuple_a) > 0:
+        a1 = tuple_a[0]
+        if len(tuple_a) > 1:
+            a2 = tuple_a[1]
+    if len(tuple_b) > 0:
+        b1 = tuple_b[0]
+        if len(tuple_b) > 1:
+            b2 = tuple_b[1]
+    return (a1 + b1, a2 + b2)
