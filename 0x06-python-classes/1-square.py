252dda96959bbf3d9e204d73976adb454e11d05a
mrtwumgh <twumstephen2006@gmail.com>
2023-07-06 06:27:28 +0000

    1-square.py added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module that defines a square
+"""
+
+
+class Square:
+    """
+    A class square
+    Args:
+        size: Size of the square
+    Attributes:
+        size: Size of the square
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
