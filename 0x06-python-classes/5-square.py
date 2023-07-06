c9931285f4a063b63c1cb946f35ce954969c3af6
mrtwumgh <twumstephen2006@gmail.com>
2023-07-06 08:16:41 +0000

    5-square.py added

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+A module that defines a square
+"""
+
+
+class Square:
+    """
+    A class that defines a square
+
+    Args:
+        size: size of the square
+    Attributes:
+        size: size of the square
+
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """
+        getter for size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter for size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        returns the area of the square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        prints out the square
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
