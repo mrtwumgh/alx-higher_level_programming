7de851979e930e39d396c7b6f8fa6dc47491f7cc
mrtwumgh <twumstephen2006@gmail.com>
2023-07-11 08:47:45 +0000

    fix for tests/6-max_integer_test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -18,12 +18,40 @@ class TestMaxInteger(unittest.TestCase):
         pos_ints = [1, 2, 3, 4]
         self.assertEqual(max_integer(pos_ints), 4)
 
+    def test_max_at_beginning(self):
+        """
+        tests max at beginning
+        """
+        max_at_beg = [4, 1, 2, 3]
+        self.assertEqual(max_integer(max_at_beg), 4)
+
+    def test_max_at_middle(self):
+        """
+        tests max in the middle
+        """
+        max_in_mid = [1, 2, 5, 3, 4]
+        self.assertEqual(max_integer(max_in_mid), 5)
+
+    def test_one_neg(self):
+        """
+        tests one negative number in list
+        """
+        one_neg = [1, 2, -3, 4, 5]
+        self.assertEqual(max_integer(one_neg), 5)
+
     def test_negative_integers(self):
         """
         test if list items are negative
         """
-        neg_ints = [1, 3, 4, 6]
-        self.assertEqual(max_integer(neg_ints), 6)
+        neg_ints = [-1, -3, -4, -6]
+        self.assertEqual(max_integer(neg_ints), -1)
+
+    def test_one_element(self):
+        """
+        test one element
+        """
+        one_elem = [3]
+        self.assertEqual(max_integer(one_elem), 3)
 
     def test_floats(self):
         """
