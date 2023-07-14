e9125281e64def774c1f2112c6063a94520fb8a1
mrtwumgh <twumstephen2006@gmail.com>
2023-07-14 08:57:35 +0000

    2-is_same_class.py added

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a module that returns true
+if an object is an instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    a function that returns true
+    if an object is an instance of a class
+    """
+    return type(obj) == a_class
