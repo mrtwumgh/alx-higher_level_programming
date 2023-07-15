ecbc82933fb6fb646a6bccb7f9e645191b8b6602
mrtwumgh <twumstephen2006@gmail.com>
2023-07-15 08:09:32 +0000

    files added

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A module that defines classes
+"""
+
+
+class BaseGeometry:
+    """
+    A class that defines an area
+    """
+    def area(self):
+        """
+        raises an exception with the message
+        area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function that validates a value
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    A class that defines a rectangle
+    """
+    def __init__(self, width, height):
+
