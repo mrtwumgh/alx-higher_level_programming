ab4adacf7cce663d174afbe230dc953beea0f4f5
mrtwumgh <twumstephen2006@gmail.com>
2023-07-18 13:00:45 +0000

    models/base.py and tests updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,16 @@ class Base:
             return []
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(list_dicts))
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,7 @@ Tests for the class base
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBaseClass(unittest.TestCase):
@@ -63,6 +64,21 @@ class TestBaseClass(unittest.TestCase):
 
         list_dictionaries = None
         self.assertEqual(Base.to_json_string(list_dictionaries), [])
+
+    def test_save_to_file(self):
+        """
+        tests the save_to_file method
+        """
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        s1 = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8},'
+        s2 = ' {"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}]'
+        output = s1 + s2
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), output)
+
 
 if __name__ == "__main__":
     unittest.main()
