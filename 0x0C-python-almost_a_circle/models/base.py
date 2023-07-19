6bec3eabadd6d5f1a7a0eb91956bdb2415cf5553
mrtwumgh <twumstephen2006@gmail.com>
2023-07-19 08:23:35 +0000

    models/base.py and tests updaed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,7 @@ class Base:
         returns the JSON string representation
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
@@ -66,3 +66,16 @@ class Base:
         dummy.update(**dictionary)
         return dummy
 
+    @classmethod
+    def load_from_file(cls):
+        """
+        returns a list of instances
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as f:
+                content = f.read()
+                list_of_dicts = cls.from_json_string(content)
+                return [cls.create(**d) for d in list_of_dicts]
+        except FileNotFoundError:
+            return []
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,7 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import os
 
 
 class TestBaseClass(unittest.TestCase):
@@ -64,10 +65,10 @@ class TestBaseClass(unittest.TestCase):
         self.assertEqual(Base.to_json_string(list_dictionaries), output)
 
         list_dictionaries = []
-        self.assertEqual(Base.to_json_string(list_dictionaries), [])
+        self.assertEqual(Base.to_json_string(list_dictionaries), "[]")
 
         list_dictionaries = None
-        self.assertEqual(Base.to_json_string(list_dictionaries), [])
+        self.assertEqual(Base.to_json_string(list_dictionaries), "[]")
 
     def test_save_to_file(self):
         """
@@ -105,6 +106,29 @@ class TestBaseClass(unittest.TestCase):
         b2 = Square.create(id=24, size=5)
         self.assertEqual(b2.id, 24)
         self.assertEqual(b2.size, 5)
+
+    def test_load_from_file(self):
+        """
+        tests the load_from_file method
+        """
+        Base._Base__nb_objects = 0
+        Base.save_to_file([])
+        self.assertEqual(Base.load_from_file(), [])
+
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertEqual(len(list_rectangles_output), 2)
+
+        self.assertEqual(list_rectangles_output[0].__dict__, r1.__dict__)
+        self.assertEqual(list_rectangles_output[1].__dict__, r2.__dict__)
+
+        try:
+            os.remove("Square.json")
+        except FileNotFoundError:
+            pass
+        self.assertEqual(Square.load_from_file(), [])
 
 
 if __name__ == "__main__":
