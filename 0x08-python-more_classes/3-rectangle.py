5d5ff809c8c0f9993fd71045f713b17b5b49d030
mrtwumgh <twumstephen2006@gmail.com>
2023-10-30 10:13:25 +0000

    3-rectangle.py added

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+A class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class that defines a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Method to initialize values
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        returns the width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        method that sets width
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        returns the height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        method that sets height
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        returns the area of rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        returns the perimeter of rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        result = ""
+        for i in range(self.__height):
+            result += "#" * self.__width
+            if i < self.__height - 1:
+                result += "\n"
+        return result
+
+        return result
