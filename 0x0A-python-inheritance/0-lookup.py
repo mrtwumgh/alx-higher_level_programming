7474267e6ead89a8ae107582ad4a5f12192a0017
mrtwumgh <twumstephen2006@gmail.com>
2023-11-07 05:16:11 +0000

    0-lookup.py added

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A module that looks up available attributes
+and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    A function that looks up available attributes
+    and methods of an object
+    """
+    return dir(obj)
