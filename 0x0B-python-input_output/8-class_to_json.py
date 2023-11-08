dcd52fc0345e32c6450dcb09e66421f21bd5b941
mrtwumgh <twumstephen2006@gmail.com>
2023-11-08 07:17:11 +0000

    8-class_to_json.py added

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A function that returns the dictionary description
+with a simple data structure
+"""
+
+
+def class_to_json(obj):
+    """
+    A function that returns the dictionary description
+    with a simple data structure
+    """
+    return obj.__dict__
