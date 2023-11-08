85d6720b8f71aeedff680491070f5301976266bb
mrtwumgh <twumstephen2006@gmail.com>
2023-11-08 06:29:30 +0000

    1-write_file.py added

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that writes to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    A function that writes to a file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
