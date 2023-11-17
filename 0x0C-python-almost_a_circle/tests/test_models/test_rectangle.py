a3d22486ea358167e03c7589801f0f59cd417865
mrtwumgh <twumstephen2006@gmail.com>
2023-11-17 07:01:24 +0000

    update: models/rectangle.py area method added, tests/test_models/test_rectangle.py tests for area method added

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,6 +18,7 @@ class TestRectangleClass(unittest.TestCase):
         """
         self.r1 = Rectangle(10, 2, 0, 0, 12)
         self.r2 = Rectangle(3, 7, 1, 1)
+        self.r3 = Rectangle(8, 5, 2, 4)
 
     def test_attributes(self):
         """
@@ -33,7 +34,7 @@ class TestRectangleClass(unittest.TestCase):
         test case for checking id
         """
         self.assertEqual(self.r1.id, 12)
-        self.assertEqual(self.r2.id, 2)
+        self.assertEqual(self.r2.id, 5)
 
     def test_setters(self):
         """
@@ -69,6 +70,13 @@ class TestRectangleClass(unittest.TestCase):
             self.r1.y = "invalid_integer"
         with self.assertRaises(ValueError):
             self.r1.y = -3
+
+    def test_area(self):
+        """
+        tests the area method
+        """
+        self.assertEqual(self.r2.area(), 21)
+        self.assertEqual(self.r3.area(), 40)
 
     def tearDown(self):
         """
