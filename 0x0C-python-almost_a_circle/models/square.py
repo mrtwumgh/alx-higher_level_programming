bd67767fa5d09a840e9021e55e28e24382d34f79
mrtwumgh <twumstephen2006@gmail.com>
2023-11-18 06:24:05 +0000

    add: getter and setter for size in square module

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,7 +14,29 @@ class Square(Rectangle):
         """
         Initializes the attributes
         """
+        self.__width = size
+        self.__height = size
+        self.__x = x
+        self.__y = y
         Rectangle.__init__(self, size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+        returns the size
+        """
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the value for size
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        else:
+            self.__width = value
+            self.__height = value
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
