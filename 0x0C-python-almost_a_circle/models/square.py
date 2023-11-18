f3dbd631fccf180d1184a2d0dd00cc06c22205bc
mrtwumgh <twumstephen2006@gmail.com>
2023-11-18 06:51:33 +0000

    update: update method modified and str magic method modified

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,10 +14,9 @@ class Square(Rectangle):
         """
         Initializes the attributes
         """
-        self.__width = size
-        self.__height = size
-        self.__x = x
-        self.__y = y
+        self.size = size
+        self.x = x
+        self.y = y
         Rectangle.__init__(self, size, size, x, y, id)
 
     @property
@@ -34,9 +33,35 @@ class Square(Rectangle):
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        else:
-            self.__width = value
-            self.__height = value
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        """
+        updates attribute values
+        """
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.size = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]
+        else:
+            for key,value in kwargs.items():
+                if key == "id":
+                    self.id = kwargs[key]
+                if key == "size":
+                    self.size = kwargs[key]
+                if key == "x":
+                    self.x = kwargs[key]
+                if key == "y":
+                    self.y = kwargs[key]
