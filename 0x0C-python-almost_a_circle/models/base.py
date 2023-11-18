7f1f6b2f2507f15dac09e13e0a15671bb76276be
mrtwumgh <twumstephen2006@gmail.com>
2023-11-18 14:54:48 +0000

    add: save_to_file method added and various tests added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,23 @@ class Base:
         """
         returns the json string representation
         """
+        list_dicts = []
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the string representation to file
+        """
+        list_dicts = []
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                for objs in list_objs:
+                    list_dicts.append(objs.to_dictionary())
+                file.write(cls.to_json_string(list_dicts))
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,10 @@ A module to test the base class
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import os
+import json
 
 
 class TestBaseClass(unittest.TestCase):
@@ -51,15 +55,52 @@ class TestBaseClass(unittest.TestCase):
         """
         dicts = [{
             "id": 1,
-            "name": "Alice"
+            "name": "Al"
             },
             {
             "id": 2,
-            "name": "James"
+            "name": "Jam"
         }]
         result = Base.to_json_string(dicts)
-        expected_result = '[{"id": 1, "name": "Alice"}, {"id": 2, "name": "James"}]'
+        expected_result = '[{"id": 1, "name": "Al"}, {"id": 2, "name": "Jam"}]'
         self.assertEqual(result, expected_result)
+
+    def test_save_to_file_empty(self):
+        """
+        tests an empty list
+        """
+        Base.save_to_file([])
+        self.assertTrue(os.path.exists("Base.json"))
+        with open("Base.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+    def test_save_to_file_none(self):
+        """
+        tests case for none input
+        """
+        Base.save_to_file(None)
+        self.assertTrue(os.path.exists("Base.json"))
+        with open("Base.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+    def test_save_to_file_valid(self):
+        """
+        test a valid input
+        """
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        self.assertTrue(os.path.exists("Rectangle.json"))
+        with open("Rectangle.json", "r") as file:
+            content = json.load(file)
+            self.assertEqual(content[0]['width'], 10)
+            self.assertEqual(content[0]['height'], 7)
+            self.assertEqual(content[0]['x'], 2)
+            self.assertEqual(content[0]['y'], 8)
+            self.assertEqual(content[1]['width'], 2)
+            self.assertEqual(content[1]['height'], 4)
+            self.assertEqual(content[1]['x'], 0)
+            self.assertEqual(content[1]['y'], 0)
 
     def tearDown(self):
         """
@@ -67,6 +108,10 @@ class TestBaseClass(unittest.TestCase):
         """
         del self.b1
         del self.b2
+        if os.path.exists("Base.json"):
+            os.remove("Base.json")
+        if os.path.exists("Rectanggle.json"):
+            os.remove("Rectangle.json")
 
 
 if __name__ == "__main__":
