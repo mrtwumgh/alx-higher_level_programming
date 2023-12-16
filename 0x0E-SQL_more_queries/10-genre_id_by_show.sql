efcc08e84ff1188e5aaff1393d77479c9058b3c3
mrtwumgh <twumstephen2006@gmail.com>
2023-12-16 08:26:09 +0000

    add: 10-genre_id_by_show added

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- a script that lists all shows contained in the hbtn_0d_tvshows
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+JOIN tv_shows_genres WHERE tv_shows.id=tv_shows_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genre.genre_id;
