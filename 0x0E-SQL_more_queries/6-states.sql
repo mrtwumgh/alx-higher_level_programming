fe7148b3e6abad731926c728b6dc5768dedc5730
mrtwumgh <twumstephen2006@gmail.com>
2023-12-16 07:43:47 +0000

    update: passed the database to the CREATE TABLE command

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256));
